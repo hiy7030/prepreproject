10b7649d4c296574cccd6d4d08ee825a3000ec63
hiy7030 <hiy7030@gmail.com>
2022-11-21 15:48:19 +0900

    Basic settinds

---
+++ src/main/java/com/prepreproject/PrepreprojectApplication.java
@@ -0,0 +1,13 @@
+package com.prepreproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepreprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepreprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prepreproject/PrepreprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prepreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepreprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
